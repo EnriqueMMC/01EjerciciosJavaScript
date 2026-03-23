0f25f1a20177331871fa489b0d7fe25b57c39851
Enrique Martínez Martínez-Checa <werte96@gmail.com>
2026-03-23 12:03:23 +0100

    He hecho hasta el ejercicio 6 de JavaScript

--- 05-Operadores/js/app.js
+++ 05-Operadores/js/app.js
@@ -45,8 +45,10 @@ function parImpar() {
 // Muestra los resultados en la consola.
 
 function comparaciones() {
-    let num1 = Math.random() * 10;
-    let num2 = Math.random() * 10;
+    let num1 = Math.round(Math.random() * 10);
+    let num2 = Math.round(Math.random() * 10);
+    console.log(num1);
+    console.log(num2);
 
     if(num1 === num2) {
         console.log("Ambos números son iguales")
@@ -61,17 +63,54 @@ function comparaciones() {
     
 }
 
-//aaa bbb
-
 //comparaciones();
+
 
 //4. Crea un programa que solicite al usuario su edad y determine si es mayor de edad (18 años o más) utilizando un operador lógico.
 
+function mayorEdad() {
+    let edad = prompt("Introduzca su edad: ");
+    edad = parseInt(edad);
 
+    if(edad >= 18){
+        console.log("Es mayor de edad");
+    } else {
+        console.log("No es mayor de edad");
+    }
+}
+
+//mayorEdad();
 
 //5. Declara tres variables booleanas y utiliza operadores lógicos (&&, ||, !) para evaluar diferentes combinaciones. Muestra los resultados en la consola.
 
-//6. Escribe un programa que calcule el área de un triángulo. Solicita al usuario la base y la altura, y utiliza el operador de multiplicación (*) y división (/).
+function operadoresLogicos() {
+    let primero = true;
+    let segundo = false;
+    let tercero = true;
+
+    if(primero === true || segundo === true){
+        console.log("El primero y/o el segundo son true");
+        if(primero && segundo){
+            console.log("Los dos son true")
+        } else if(primero && !segundo){
+            console.log("Solo el primero es true");
+        }
+    }
+}
+
+//operadoresLogicos();
+
+//6. Escribe un programa que calcule el área de un triángulo. Solicita al usuario la base y la altura, y utiliza el operador de multiplicación (*) y 
+// división (/).
+
+function areaTriangulo() {
+    let b = prompt("Escriba la base del triángulo: ");
+    let h = prompt("Escriba la altura del triángulo: ");
+    let pi = Math.pi;
+    console.log(pi);
+}
+
+areaTriangulo();
 
 //7. Declara una variable con un número inicial y utiliza operadores de incremento (++) y decremento (--) para modificar su valor. Muestra el resultado después de cada operación.
 
